5a170afbb8f0e0f52bda8d378c716b98848feb08
hernanbarrientos <hrn.2211@gmail.com>
2023-01-09 19:13:02 -0300

    Criada a lógica da ResultActivity

--- app/src/main/java/com/example/conversordemedidas/ResultActivity.kt
+++ app/src/main/java/com/example/conversordemedidas/ResultActivity.kt
@@ -2,10 +2,27 @@ package com.example.conversordemedidas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+
+        val result = intent.getDoubleExtra("RESULT", 0.0)
+        val label = intent.getStringExtra("LABEL")
+
+        val tvValue: TextView = findViewById(R.id.tvValue)
+        tvValue.text = result.toString()
+
+        val tvValueLabel: TextView = findViewById(R.id.tvValueLabel)
+        tvValueLabel.text = label
+
+        val btnClose : Button = findViewById(R.id.btnClose)
+        btnClose.setOnClickListener {
+            finish()
+        }
     }
 }
